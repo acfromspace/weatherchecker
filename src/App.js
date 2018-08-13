7aa218989bdee2ba7bdf34e431cc582dec77cba7
Andrew De Leon <andrewchristopherdeleon@gmail.com>
2018-08-12 18:41:11 -0700

    [+] Components and starting to put API

--- src/App.js
+++ src/App.js
@@ -2,20 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+import { userInfo } from 'os';
+
+// Create an instance of the application that creates a component via React
+
+const API_KEY = '3d0ef9639326e8f751a89d6ef01a91bf';
+
+class App extends React.Component {
+  // One can put their own methods here
+  getWeather = async () => {
+    const API_CALL = await fetch();
+  }
+
+  // Before one had to do this to properly bind the function React version < 16.0
+  // constructor() {
+  //   this.getWeather = this.getWeather.bind(this);
+  // }
+
+  // Render is a built in method in order to show output to the user
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Titles/>
+        <Form/>
+        <Weather/>
       </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
     );
   }
 }
+
+// Required for index.js to be aware of this
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Render this component (App) to this root element which is getElementById('root')
+// ReactDOM.render() takes two arguments, first being the app, second being the div
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
