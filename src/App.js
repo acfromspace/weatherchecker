1dd231b40294c68d631aa6de61bead2859c7a3c7
Andrew De Leon <andrewchristopherdeleon@gmail.com>
2018-08-18 20:27:05 -0700

    [*] Console log displays weather information

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
-import { userInfo } from 'os';
 
 // Create an instance of the application that creates a component via React
 
 const API_KEY = '3d0ef9639326e8f751a89d6ef01a91bf';
 
-class App extends React.Component {
+class App extends Component {
   // One can put their own methods here
   // JSON = Javascript Object Notation
-  getWeather = async () => {
-    const API_CALL = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    const data =  await API_CALL.json();
+  getWeather = async (e) => {
+    // Prevent the default behavior of this component
+    e.preventDefault();
+    // Variable that makes the call to the URL
+    const API_CALL = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+    // Convert the response from the URL to json format (json is a universal accepted language)
+    const data = await API_CALL.json();
+    // Display the data
     console.log(data);
   }
 
@@ -29,9 +32,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Titles/>
-        <Form/>
-        <Weather/>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather />
       </div>
       // <div className="App">
       //   <header className="App-header">
