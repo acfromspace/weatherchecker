7f2ef95d169b999e9cb91bca4cc45ebc032583ca
Andrew De Leon <andrewchristopherdeleon@gmail.com>
2018-08-18 19:15:38 -0700

    [*] App.js, Form.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,11 @@ const API_KEY = '3d0ef9639326e8f751a89d6ef01a91bf';
 
 class App extends React.Component {
   // One can put their own methods here
+  // JSON = Javascript Object Notation
   getWeather = async () => {
-    const API_CALL = await fetch();
+    const API_CALL = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data =  await API_CALL.json();
+    console.log(data);
   }
 
   // Before one had to do this to properly bind the function React version < 16.0
