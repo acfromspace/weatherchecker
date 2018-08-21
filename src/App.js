86205c55ae1611a76447e3adeea2b702990b5305
Andrew De Leon <andrewchristopherdeleon@gmail.com>
2018-08-21 16:24:05 -0700

    [*] Barebones finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Titles from './components/Titles';
-import Form from './components/Form';
-import Weather from './components/Weather';
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 // Create an instance of the application that creates a component via React
 
-const API_KEY = '3d0ef9639326e8f751a89d6ef01a91bf';
+const API_KEY = "3d0ef9639326e8f751a89d6ef01a91bf";
 
 class App extends Component {
   // constructor(props) {
@@ -23,32 +23,47 @@ class App extends Component {
     humidity: undefined,
     description: undefined,
     error: undefined
-  }
+  };
   // One can put their own methods here
   // JSON = Javascript Object Notation
-  getWeather = async (e) => {
+  getWeather = async e => {
     // Prevent the default behavior of this component
     e.preventDefault();
+    // Input from the user
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     // Variable that makes the call to the URL
-    const API_CALL = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const API_CALL = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
     // Convert the response from the URL to json format (json is a universal accepted language)
     const data = await API_CALL.json();
-    // Display the data
-    console.log(data);
-    // Have the state variables equal to the proper data calls
-    // This is how to manipulate APIs
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      // In this API, it uses an array for the description
-      description: data.weather[0].description,
-      error: "",
-    });
-  }
+    // If both return true, then render the code
+    if (city && country) {
+      // // Display the data
+      // console.log(data);
+      // Have the state variables equal to the proper data calls
+      // This is how to manipulate APIs
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        // In this API, it uses an array for the description
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value."
+      });
+    }
+  };
 
   // Back in time, one had to do this to properly bind the function React version < 16.0
   // constructor() {
@@ -67,7 +82,8 @@ class App extends Component {
           country={this.state.country}
           humidity={this.state.humidity}
           description={this.state.description}
-          error={this.state.error} />
+          error={this.state.error}
+        />
       </div>
       // <div className="App">
       //   <header className="App-header">
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,12 @@ class Weather extends Component {
     render() {
         return (
             <div>
-                <p>Location: {this.props.city}{this.props.country}</p>
-                <p>Temperature: {this.props.temperature}</p>
-                <p>Humidity: {this.props.humidity}</p>
-                <p>Description: {this.props.description}</p>
+                {/* The && functions as if "Only perform if all are true" */}
+                { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+                { this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                { this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                { this.props.description && <p>Description: {this.props.description}</p>}
+                { this.props.error && <p>{ this.props.error }</p>}
                 {/* Wind speed perhaps? */}
             </div>
         );
